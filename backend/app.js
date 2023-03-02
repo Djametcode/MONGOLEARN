97b3bdcb064105c0bcd73d7fb46a178fada2092e
djametcode <kampretsiji1@gmail.com>
2023-03-03 02:40:52 +0700

    changes

--- backend/app.js
+++ backend/app.js
@@ -14,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/v3/user", router);
 
+ServerlessHttp(app);
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URL);
