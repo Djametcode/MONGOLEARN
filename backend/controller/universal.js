79a48184baf2bd2bac25032ecedc1148ffdda01f
djametcode <kampretsiji1@gmail.com>
2023-03-09 23:53:55 +0700

    changes

--- backend/controller/universal.js
+++ backend/controller/universal.js
@@ -1,7 +1,7 @@
 const DataScheme = require("../model/Learn");
 
 const getAllSecretMessage = async (req, res) => {
-  const data = await DataScheme.find({}).sort("date");
+  const data = await DataScheme.find({}).sort("-date");
   res.status(200).json({ data });
 };
 
