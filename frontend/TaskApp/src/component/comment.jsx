18909ae0546d1f78a0d91b9e778c837b16b3bf1d
djametcode <kampretsiji1@gmail.com>
2023-04-12 16:01:00 +0700

    changes

--- frontend/TaskApp/src/component/comment.jsx
+++ frontend/TaskApp/src/component/comment.jsx
@@ -34,9 +34,7 @@ const CommentJsx = () => {
 
   const getAllComment = async () => {
     try {
-      const response = await axios.get(
-        `https://grumpy-worm-stockings.cyclic.app/api/v3/u/comment/${id}`
-      );
+      const response = await axios.get("http://localhost:3000/api/v3/u/list");
       const item = response.data;
       const { data } = item;
       console.log(data);
