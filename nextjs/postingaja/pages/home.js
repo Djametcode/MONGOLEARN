6f03357d43f3dd9dac37934062604e1b443ed830
djametcode <kampretsiji1@gmail.com>
2023-04-22 22:32:14 +0700

    changes

--- nextjs/postingaja/pages/home.js
+++ nextjs/postingaja/pages/home.js
@@ -4,8 +4,19 @@ export default function ProductList({ data }) {
   console.log(data);
   return (
     <div>
-      <div className=" bg-purple-700 p-4 text-3xl text-white">
+      <div className=" flex justify-between sticky top-0 z-10 bg-purple-700 p-4 text-3xl text-white">
         <h1 className=" font-quick">Product List</h1>
+        <div className=" flex flex-col justify-center">
+          <svg
+            className=" w-7 h-7"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+          >
+            <path d="M1 1.75A.75.75 0 011.75 1h1.628a1.75 1.75 0 011.734 1.51L5.18 3a65.25 65.25 0 0113.36 1.412.75.75 0 01.58.875 48.645 48.645 0 01-1.618 6.2.75.75 0 01-.712.513H6a2.503 2.503 0 00-2.292 1.5H17.25a.75.75 0 010 1.5H2.76a.75.75 0 01-.748-.807 4.002 4.002 0 012.716-3.486L3.626 2.716a.25.25 0 00-.248-.216H1.75A.75.75 0 011 1.75zM6 17.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM15.5 19a1.5 1.5 0 100-3 1.5 1.5 0 000 3z" />
+          </svg>
+        </div>
       </div>
       <div className=" font-quick text-white gap-2 flex flex-wrap justify-center mt-2">
         {data.products.map((item) => (
