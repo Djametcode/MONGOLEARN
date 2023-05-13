9177b148ef5cd9dbf5d7fa393d1cad6c76580947
djametcode <kampretsiji1@gmail.com>
2023-05-13 14:20:21 +0700

    changes

--- nextjs/postingaja/pages/landing.jsx
+++ nextjs/postingaja/pages/landing.jsx
@@ -92,19 +92,21 @@ export default function Home() {
             <h1 className=" text-2xl font-extrabold">Beranda</h1>
           </div>
           <div className=" flex flex-col justify-center max-sm:translate-x-7">
-            <svg
-              className=" w-10 bg-gray-400/40 p-2 rounded-full"
-              fill="grey"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-              aria-hidden="true"
-            >
-              <path
-                clipRule="evenodd"
-                fillRule="evenodd"
-                d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
-              />
-            </svg>
+            <div className=" bg-slate-400/50 rounded-full">
+              <svg
+                className=" w-10 p-2 rounded-full"
+                fill="grey"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+              >
+                <path
+                  clipRule="evenodd"
+                  fillRule="evenodd"
+                  d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+                />
+              </svg>
+            </div>
           </div>
         </div>
       </div>
