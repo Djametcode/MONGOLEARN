599dfa4d5bf6922d3505839b2a4d75ee335168f1
djametcode <kampretsiji1@gmail.com>
2023-05-14 10:48:54 +0700

    changes

--- nextjs/postingaja/pages/search.jsx
+++ nextjs/postingaja/pages/search.jsx
@@ -2,12 +2,12 @@ import Layout from "@/components/layout";
 
 export default function Search() {
   return (
-    <div className=" h-full flex flex-col justify-center text-white">
+    <div className=" flex justify-center translate-y-72 text-white">
       <h1>Update soon</h1>
     </div>
   );
 }
 
-Search.getlayout = function getLayout(page) {
+Search.getLayout = function getLayout(page) {
   return <Layout>{page}</Layout>;
 };
